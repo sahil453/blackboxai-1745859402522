608d0549e3c2529816812f1adbe435194fb6031a
sahil453 <122201255+sahil453@users.noreply.github.com>
2025-04-28 22:27:08 +0530

    Add frontend/src/App.js 1745859429030

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AuthPage from './pages/AuthPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthPage />} />
+        {/* Additional routes like DishDetail, RestaurantDetail, Profile can be added here */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
